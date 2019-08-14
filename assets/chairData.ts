8d6b39e41a3e1b8ce43837767eb1d12f848a1a5b
malekga96 <52546645+malekga96@users.noreply.github.com>
2019-08-14 09:59:41 -0400

    Add files via upload

---
+++ assets/chairData.ts
@@ -0,0 +1,9 @@
+import {Chair} from '../app/chair';
+
+export const MYCHAIR: Chair[] =
+[
+    {brand: 'NIKE', make:'Athletic', country:'Canada'},
+    {brand: 'GT', make:'Casual', country:'USA'},
+    {brand: 'Adidas', make:'Office', country:'France'}
+    
+]
